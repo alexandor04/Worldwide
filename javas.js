a08e720ec116a8ed0e6c8e640d4473e774aa85b0
alexandor04 <stempfel.alex@gmail.com>
2024-05-18 15:34:52 +0200

    Update javas.js

--- javas.js
+++ javas.js
@@ -9,3 +9,12 @@ var ghost_key = '17e7f410823c30ba9f7a89dd5e';
 /* Disqus */
 var disqus_shortname = 'biron-demo'; // Replace 'biron-demo' with your disqus account shortname
     
+(function(h,o,t,j,a,r){
+    h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
+    h._hjSettings={hjid:2660609,hjsv:6};
+    a=o.getElementsByTagName('head')[0];
+    r=o.createElement('script');r.async=1;
+    r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
+    a.appendChild(r);
+})(window,document,'https://static.hotjar.com/c/hotjar-','.js?sv=');
+
