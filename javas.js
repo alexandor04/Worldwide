7f417c81fd1c6e6aaeabf81025e216ca4f8dabe8
alexandor04 <stempfel.alex@gmail.com>
2024-05-18 15:13:57 +0200

    Update javas.js

--- javas.js
+++ javas.js
@@ -1,1 +1,11 @@
+// The number of pages available for loading more posts
+var maxPages = parseInt('34');
+/* Ghost Search */
+// Replace it with your domain: ghost_host = 'https://yoursite.domain'
+var ghost_host = 'https://roughtrade.ghost.io';
+// Settings > Integrations > New Custom Integration. Copy Key. Replace ghost_key below.
+var ghost_key = '17e7f410823c30ba9f7a89dd5e';
 
+/* Disqus */
+var disqus_shortname = 'biron-demo'; // Replace 'biron-demo' with your disqus account shortname
+    
