08783594ed8b9479d22978e38fa17b0788802654
alexandor04 <stempfel.alex@gmail.com>
2024-05-18 15:46:19 +0200

    Update javas.js

--- javas.js
+++ javas.js
@@ -18,3 +18,23 @@ var disqus_shortname = 'biron-demo'; // Replace 'biron-demo' with your disqus ac
     a.appendChild(r);
 })(window,document,'https://static.hotjar.com/c/hotjar-','.js?sv=');
 
+window.dataLayer = window.dataLayer || [];
+function gtag(){dataLayer.push(arguments);}
+gtag('js', new Date());
+
+gtag('config', 'G-XSFQRHW56P');
+
+(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-M3BGVC2');/*]]>*/
+
+(function(window, document) {
+    //var path      = window.location.pathname.match(/[^\/]./)[0];
+    var path      = window.location.pathname.match(/\/(gb|us)\/?/i)[1];
+    var home_icon = document.getElementsByClassName("header__brand-home")[0];
+    home_icon.setAttribute('href',  home_icon.getAttribute('href') + '/' + path);
+})(window, document);
+
+
