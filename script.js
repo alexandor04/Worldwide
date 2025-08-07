9744c7ff213c62deeef1abab8c89457fb81fc2f4
alexandor04 <stempfel.alex@gmail.com>
2025-08-07 21:21:51 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 <script type="module">
-  // Import the functions you need from the SDKs you ne
+  // Import the functions you need from the SDKs you nee
   import { initializeApp } from "https://www.gstatic.com/firebasejs/12.0.0/firebase-app.js";
   // TODO: Add SDKs for Firebase products that you want to use
   // https://firebase.google.com/docs/web/setup#available-libraries
